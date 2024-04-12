4f7b6fc1df332c22349f5209080ca654701b5e97
alex_lima_equise <113062275+Minjjo@users.noreply.github.com>
2024-04-12 17:27:50 -0400

    borrado de ;

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
 export const Button = props => {
-    const {text} = props;
+    const {text} = props
     const [count, setcount] = useState(0);
     function handlerButton() {
-        setcount(count + 1 );
+        setcount(count + 1 )
     } 
     return (
         <button onClick={handlerButton}>
