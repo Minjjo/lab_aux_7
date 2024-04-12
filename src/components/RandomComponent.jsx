0221acf99a1b6de981bf1f9e966e3667432d4af8
alex lima <alexlimaequise5@gmail.com>
2024-04-12 18:06:36 -0400

    pregunta 4

---
+++ src/components/RandomComponent.jsx
@@ -0,0 +1,5 @@
+export const RandomComponent = () => {
+    return (
+        <img src="https://th.bing.com/th/id/R.6701130abc1feda026a63ff3674cc871?rik=M%2bjQrPaxf6McPw&pid=ImgRaw&r=0&sres=1&sresct=1" alt="uatf"/>
+    )
+}
