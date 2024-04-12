df5e2cfbd285194b8861895f5e1175daf09ba9d4
alex lima <alexlimaequise5@gmail.com>
2024-04-12 17:24:56 -0400

    primer_commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { WapperList } from './components/WapperList'
+import { List } from './components/List'
+function App() {
+  return (
+    <>
+    <WapperList>
+      <List/>
+    </WapperList>  
+    </>
+    
+  )
+}
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export const Button = props => {
+    const {text} = props;
+    const [count, setcount] = useState(0);
+    function handlerButton() {
+        setcount(count + 1 );
+    } 
+    return (
+        <button onClick={handlerButton}>
+            {text}{count}
+        </button>
+    )
+}
